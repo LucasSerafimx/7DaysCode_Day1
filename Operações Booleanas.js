b72947f9c6d46d509f2cb085a329858b8216de85
LucasSerafimx <lucas.serafimx@gmail.com>
2024-01-22 09:26:03 -0300

    Day1_Complete

---
+++ Operações Booleanas.js
@@ -0,0 +1,28 @@
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+
+// == (Igual a) -> Não precisa ser o tipo igual.
+// === (Identico) -> Tipo e informações precisam ser igual.
+
+if(numeroUm == stringUm){
+    console.log("As variaveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+} else{
+    console.log("As variaveis numeroUm e stringUm não tem o masmo valor")
+}
+
+if(numeroTrinta == stringTrinta){
+    console.log("As variaveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+} else{
+    console.log("As variaveis numeroUm e stringUm não tem o masmo valor")
+}
+
+if(numeroDez == stringDez){
+    console.log("As variaveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+} else{
+    console.log("As variaveis numeroUm e stringUm não tem o masmo valor")
+}
